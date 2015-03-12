8f1536d828ea8161b456f150a1920148582a0855
Liam Bennett <liam@littleowllabs.com>
2015-03-12 00:13:20 +0000

    test(helpers): Make specs contingent on abilities

--- spec/stub_env/helpers_spec.rb
+++ spec/stub_env/helpers_spec.rb
@@ -11,15 +11,17 @@ describe StubEnv::Helpers do
 
     include_examples 'stub_env tests'
 
-    context "with the verify partial doubles option set" do
+    if RSpec::Mocks::Configuration.public_instance_methods.include? :verify_partial_doubles=
+      context "with the verify partial doubles option set" do
 
-      before :each do
-        RSpec.configuration.mock_with :rspec do |mocks|
-          mocks.verify_partial_doubles = true
+        before :each do
+          RSpec.configuration.mock_with :rspec do |mocks|
+            mocks.verify_partial_doubles = true
+          end
         end
-      end
 
-      include_examples 'stub_env tests'  
+        include_examples 'stub_env tests'  
+      end
     end
   end
 end
